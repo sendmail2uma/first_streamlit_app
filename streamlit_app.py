3b5e49b54543c959a97f89a4050f149768915224
sendmail2uma <112366806+sendmail2uma@users.noreply.github.com>
2022-08-29 09:58:26 +0530

    Add breakfast and menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,9 @@
 import streamlit
 
 streamlit.title("Good Morning")
-streamlit.title("Happy Learning!")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
