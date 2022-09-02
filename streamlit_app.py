b72cde6d43f50c7b30d50cbb38b1f27ef52ad66c
sendmail2uma <112366806+sendmail2uma@users.noreply.github.com>
2022-09-02 18:22:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit Load List Contains:")
 streamlit.dataframe(my_data_rows)
 
-streamlit.header("What fruit would you like to add?")
 add_my_fruit = streamlit.text_input('What fruit would you like information about?')
 streamlit.write('Thanks for adding ', add_my_fruit)
