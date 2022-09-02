134f9e7550dacf5d878aca13fb0c4ecb0413fa21
sendmail2uma <112366806+sendmail2uma@users.noreply.github.com>
2022-09-02 18:31:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,5 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+mycur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')");
